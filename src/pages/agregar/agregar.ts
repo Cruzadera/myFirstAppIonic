57c27bf70bb0a19be20900eed23e7b66f937e7e0
Maria <mariamunozcruzado@gmail.com>
2018-12-12 20:12:38 +0100

    Finished. Code cleaned

--- src/pages/agregar/agregar.ts
+++ src/pages/agregar/agregar.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Observable} from "rxjs";
 import {AngularFireDatabase} from '@angular/fire/database';
